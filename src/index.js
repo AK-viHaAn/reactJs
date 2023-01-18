411f407873474308a0f2e001cfa8f0331ce8dae4
AK-viHaAn <kanand2607755@gmail.com>
2023-01-19 02:14:39 +0530

    Quiz Ques

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import UpperLowerCase from './Upper-LowerCase/index';
+import Quizcomponent from './OnlineExam/QuizComp';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UpperLowerCase />
+   <Quizcomponent/>
   </React.StrictMode>
-);
+); 
